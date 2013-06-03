d317454bf5f82db0336f1d5c5224a86160495ed8
Leandro Lages <leandro@siteapps.com>
2013-06-03 13:11:06 -0300

    versao inicial

---
+++ soma.js
@@ -0,0 +1,15 @@
+var cartolaBM = function() {
+   if (typeof jQuery('#jogadores-lista table').get(0) == 'undefined') {
+      alert('Você precisa está na página de seu time para que o app funcione. A página é http://cartolafc.globo.com/#!/time');
+      return
+   }
+   var e = jQuery('.ultima');
+   var t = jQuery('.ultima').prev();
+   var n = 0;
+   var r = 0;
+   for (var i = 0; i < e.length; i++) {
+      n = n + parseFloat(e[i].innerHTML);
+      r = r + parseFloat(t[i].innerHTML)
+   }
+   jQuery('#jogadores-lista table').append('<tr><td style=\'color:black; font-size:13px; background-color: #efefef\' colspan=\'7\'><strong>Médias do seu time</strong></td><td>' + r.toFixed(2) + '</td><td colspan\'2\'>' + n.toFixed(2) + '</td>')
+}();
